c516c49b4e7070dfd0fe51bc80a1c887d738f8b7
Guan <guanlin92@yahoo.com>
2016-05-13 01:30:22 -0700

    server chat is now fully functional

--- app/controllers/pusher_controller.rb
+++ app/controllers/pusher_controller.rb
@@ -1,12 +1,22 @@
 class PusherController < ApplicationController
-  def chat
-  end
-
-  def hello_world
-    Pusher.trigger('test_channel', 'my_event', {
-      message: 'hello world'
-    })
+	# skip CSRF token veritication only for chat action
+	skip_before_action :verify_authenticity_token, only: [:chat]
 	
+	def chat
+
+		user_email = params[:user_email]
+		message = params[:message]
+
+		Pusher.trigger('chat', 'sendMessage', {user_email: user_email,
+	  	message: message
+		})
+	end
+
+	def hello_world
+	#Pusher.trigger('test_channel', 'my_event', {
+	#  message: 'hello world'
+	#})
+
 	redirect_to home_index_path
-  end
+	end
 end
